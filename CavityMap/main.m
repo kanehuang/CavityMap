1767cba93ccff196a8600bba08f48ff1353241ce
CK Huang <kertmanster@gmail.com>
2015-01-12 13:51:56 +0800

    Initial Commit

---
+++ CavityMap/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CavityMap
+//
+//  Created by CKHuang on 1/12/15.
+//  Copyright (c) 2015 CKHuang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
